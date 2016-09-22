77d352c91c01709fd16d6eae0e66551bc631d6e4
Craig McCormack <craigm@littleappletech.com>
2016-09-21 23:57:39 -0600

    Add changes in index file and generator

--- generator.php
+++ generator.php
@@ -1,20 +1,16 @@
 <?php
 
-var_dump($_GET);
+//need function to pick random words
+//need function to pick random numbers
+//need function to add camelcase
+//need txt file for words
+//need numbers to choose from
+//need to accept settings from index file
+//need to return password to be displayed
+
+//accept setting from file
 
 ?>
 
 
 
-<?php /*$result = file_get_contents('http://www.paulnoll.com/Books/Clear-English/');
-		echo $result;?>
-		<?php $likes = 'eating, drinking, the  gym, nothing';
-			  $likes = explode(',' , $likes);
-			    
-			   //this turns string into an array
-			   foreach($likes as $key => $like) {
-				   echo '</br>', $like;
-			   }
- 		*/
-		
-		?>
--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 
 	<!DOCTYPE html>
 	<html>
-	<head>
-	
-		<title>P2</title>
-		<meta charset='utf8'>
+		<head>
+			<meta charset='utf8'>
+			<title>P2</title>
+		
 		
 				
-		<?php  require 'generator.php'; ?>
+			<?php  require 'generator.php'; ?>
+			<link rel='stylesheet' href='' type='text/css'>
 		
-
-		<link rel='stylesheet' href='' type='text/css'>
-		
-	</head>
-	<body>
+		</head>
+		<body>
 	
 	<!-- input, ask user how many words should be in password--
 		Text input drop down or slider, need to capture
 		how many words, need checkboxes or radio buttons to indicate--
 		if passwords should include symbols and what symbols
 		Get is good for retrieving data, Post is good for security
-		Forms need to submit to generator pages.-->
-		<form action='index.php' method='GET'>
-			Select  a number from 1 to 5:
-		<input type='number' name='quantity' min='1' max='5'>
-		<input type='checkbox' name='symbols' value='1'>
-		Do you want to include symbols?
+		Forms need to submit to generator pages.use push to push-->
+		<p>New Password: <?php echo $password; ?></p>
+		
+		<form action='index.php' method='POST'>
+			Select the number of words in your password:
+		<input type='number' name='quantity' min='1' max='25'><br />
+		<input type='checkbox' name='symbols' value='1'> Do you want to include symbols? <br />
+		<input type='checkbox' name='camelCase' value='1'> Do you want the words to be camelCase? <br />
 		<input type='submit' value='Press to enter!'>
 		</form>
 		
