7379e33948e1662f07b59eac85a6594dc8c1aee5
Craig McCormack <craigm@littleappletech.com>
2016-09-22 02:05:24 -0600

    Update all files, done for night

--- generator.php
+++ generator.php
@@ -1,24 +1,34 @@
 <?php
 
-//need function to pick random words
+//need function to pick random words - done
 //need function to pick random numbers
 //need function to add camelcase
-//need txt file for words
+//need txt file for words - done
 //need numbers to choose from
 //need to accept settings from index file
-//need to return password to be displayed
+//need to return password to be displayed - done
 
 //accept setting from index file
 
-$word_file = 'wordlist.txt';
-$words = file($word_file, FILE_IGNORE_NEW_LINES );
 
-//random word selection function
-function new_word($newword){
-	$index = array_rand($newword);	
-	return $newword[$index];
+$words = file('wordlist.txt', FILE_IGNORE_NEW_LINES );
+
+//random word selection function with number of words
+function new_word($newword, $number){
+	$newarray = [];
+	$newstring = "";	
+	$dash = "-";
+	foreach (range(0,$number) as $i){
+		$index = array_rand($newword);	
+		$newstring .=  $newword[$index] . $dash;
+	}
+	
+	
+	return rtrim($newstring, "-");
 }
-$password = new_word($words) ."-". new_word($words);
+
+
+$password = new_word($words, 15);
 
 ?>
 
