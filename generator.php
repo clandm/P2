9d056dce40dfbc839bcee4e6aeeecb679830df49
Craig McCormack <craigm@littleappletech.com>
2016-09-22 00:33:20 -0600

    Update all files

--- generator.php
+++ generator.php
@@ -8,7 +8,17 @@
 //need to accept settings from index file
 //need to return password to be displayed
 
-//accept setting from file
+//accept setting from index file
+
+$word_file = 'wordlist.txt';
+$words = file($word_file, FILE_IGNORE_NEW_LINES );
+
+//random word selection function
+function new_word($newword){
+	$index = array_rand($newword);	
+	return $newword[$index];
+}
+$password = new_word($words) ."-". new_word($words);
 
 ?>
 
--- index.php
+++ index.php
@@ -19,11 +19,12 @@
 		if passwords should include symbols and what symbols
 		Get is good for retrieving data, Post is good for security
 		Forms need to submit to generator pages.use push to push-->
-		<p>New Password: <?php echo $password; ?></p>
+		<h1>Here is Your Password: <?php echo $password; ?></h1>
 		
 		<form action='index.php' method='POST'>
 			Select the number of words in your password:
 		<input type='number' name='quantity' min='1' max='25'><br />
+		<input type='checkbox' name='symbols' value='1'> Do you want to include numbers? <br />
 		<input type='checkbox' name='symbols' value='1'> Do you want to include symbols? <br />
 		<input type='checkbox' name='camelCase' value='1'> Do you want the words to be camelCase? <br />
 		<input type='submit' value='Press to enter!'>
